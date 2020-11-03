3fe15e2c95caf59c5b737fd867e437ae2369a8b3
Thomas Perale <perale.thomas@gmail.com>
2020-11-03 20:31:10 +0100

    [add] File already implemented in huron project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # despinassy
-SQLAlchemy Database Schema collection
+
+SQLAlchemy Database Schema collection as a module. This python package is
+made to share the database schema between all the application that want to
+access the database.
---
+++ despinassy/Part.py
@@ -0,0 +1,27 @@
+from despinassy.db import db
+
+class Part(db.Model):
+    __tablename__ = 'part'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    barcode = db.Column(db.String(120))
+    name = db.Column(db.String(50))
+    counter = db.Column(db.Integer)
+
+    def __init__(self, name=None, barcode=None):
+        self.name = name
+        self.barcode = barcode
+        self.counter = 0
+
+    def __repr__(self):
+        return '<Part %r>' % (self.name)
+
+    def printed(self):
+        self.counter += 1
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'barcode': self.barcode,
+            'name': self.name,
+            'counter': self.counter,
+        }
