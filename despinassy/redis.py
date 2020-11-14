9d8bc45756131b34bb71d8913e8df53a7dce84e7
Thomas Perale <perale.thomas@gmail.com>
2020-11-14 12:10:14 +0100

    Update redis msg format

--- despinassy/redis.py
+++ despinassy/redis.py
@@ -1,2 +1,7 @@
+from collections import namedtuple
+
+BarcodeMessage = namedtuple('BarcodeMessage', 'barcode, origin, redis')
+RedisPrintMessage = namedtuple('BarcodeMessage', 'barcode, origin, redis, name')
+
 def redis_subscribers_num(redis, channel):
     return redis.execute_command('PUBSUB', 'NUMSUB', channel)[1]
