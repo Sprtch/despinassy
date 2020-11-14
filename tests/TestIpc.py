699fafdccfd9b5d8f01fcc7607ea17a1fdd3114c
Thomas Perale <perale.thomas@gmail.com>
2020-11-14 17:35:59 +0100

    [fix] Changed name of ipc

--- tests/TestIpc.py
+++ tests/TestIpc.py
@@ -1,14 +1,14 @@
 import unittest
-from despinassy.redis import RedisPrintMessage, redis_create_print_message, redis_subscribers_num
+from despinassy.ipc import IpcPrintMessage, ipc_create_print_message, redis_subscribers_num
 from collections import namedtuple
 
 class TestRedis(unittest.TestCase):
     def test_msg_cast(self):
         TestMessage = namedtuple('PrintMessage', 'barcode, origin, redis, name')
         msg = TestMessage(barcode="1234", origin="here", redis="redis", name="1234")
-        a = redis_create_print_message(msg)
+        a = ipc_create_print_message(msg)
 
-        self.assertTrue(isinstance(a, RedisPrintMessage))
+        self.assertTrue(isinstance(a, IpcPrintMessage))
 
 if __name__ == '__main__':
     unittest.main()
