0ebeb2d7ab211c31f03a91676201ef84adfe4d50
Thomas Perale <perale.thomas@gmail.com>
2020-11-21 22:04:27 +0100

    [add] testing delete

--- test/test_part.py
+++ test/test_part.py
@@ -15,23 +15,30 @@ class TestDatabasePart(unittest.TestCase):
     def tearDownClass(self):
         db.drop_all()
 
-    def test_part_creation(self):
+    def test_part_1_creation(self):
         p = Part(name="BARCODE", barcode="QWERTY1234")
         db.session.add(p)
         db.session.commit()
         self.assertEqual(Part.query.count(), 1)
 
-    def test_part_query(self):
+    def test_part_2_query(self):
         p = Part.query.filter(Part.barcode == "QWERTY1234").first()
         self.assertEqual(p.id, 1)
         self.assertEqual(p.name, "BARCODE")
         self.assertEqual(p.barcode, "QWERTY1234")
         self.assertEqual(p.counter, 0)
 
-    def test_part_unique(self):
+    def test_part_3_unique(self):
         p = Part(name="BARCODE2", barcode="QWERTY1234")
         db.session.add(p)
         self.assertRaises(sqlalchemy.exc.IntegrityError, db.session.commit)
+        db.session.rollback()
+
+    def test_part_4_delete(self):
+        self.assertNotEqual(Part.query.count(), 0)
+        Part.query.delete()
+        db.session.commit()
+        self.assertEqual(Part.query.count(), 0)
 
 if __name__ == '__main__':
     unittest.main()
