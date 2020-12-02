27d21f2ae36e5149044bec79a6280f555980d2e7
Thomas Perale <perale.thomas@gmail.com>
2020-12-02 20:15:44 +0100

    [fix] test inventory

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -15,49 +15,59 @@ class TestDatabaseInventory(unittest.TestCase):
     def tearDownClass(self):
         db.drop_all()
 
-    def test_inventory_creation_1(self):
-        BARCODE = "QWERTY1234"
-        NAME = "BARCODE"
-        p = Part(name=NAME, barcode=BARCODE)
+    def tearDown(self):
+        Inventory.query.delete()
+        Part.query.delete()
+
+    @staticmethod
+    def inventory_creation(name, barcode):
+        p = Part(name=name, barcode=barcode)
         db.session.add(p)
         i = Inventory(part=p)
         db.session.add(i)
         db.session.commit()
-        self.assertEqual(Inventory.query.count(), 1)
-
-    def test_inventory_creation_2(self):
-        BARCODE = "HELLO1234"
-        NAME = "BARCODE 2"
-        p = Part(name=NAME, barcode=BARCODE)
-        db.session.add(p)
-        i = Inventory(part=p)
-        db.session.add(i)
-        db.session.commit()
-
-        self.assertEqual(len(Part.query.filter(Part.barcode == BARCODE).all()), 1)
-
-        # Inventory.create_inventory_from_barcode("HELLO1234")
-
-        self.assertEqual(Inventory.query.count(), 2)
 
     def test_inventory_query_1(self):
+        self.assertEqual(Inventory.query.count(), 0)
+        TestDatabaseInventory.inventory_creation("BARCODE", "QWERTY1234")
+        self.assertEqual(Inventory.query.count(), 1)
+        TestDatabaseInventory.inventory_creation("BARCODE 2", "HELLO1234")
+        self.assertEqual(len(Part.query.filter(Part.barcode == "HELLO1234").all()), 1)
+        self.assertEqual(Inventory.query.count(), 2)
+
         p = Part.query.filter(Part.barcode == "QWERTY1234").first()
-        
         self.assertEqual(len(p.inventories), 1)
 
     def test_inventory_query_2(self):
+        self.assertEqual(Inventory.query.count(), 0)
+        TestDatabaseInventory.inventory_creation("BARCODE", "QWERTY1234")
+        self.assertEqual(Inventory.query.count(), 1)
+        TestDatabaseInventory.inventory_creation("BARCODE 2", "HELLO1234")
+        self.assertEqual(len(Part.query.filter(Part.barcode == "HELLO1234").all()), 1)
+        self.assertEqual(Inventory.query.count(), 2)
+
         p = Part.query.filter(Part.barcode == "QWERTY1234").first()
         i = p.inventories[0]
         self.assertEqual(i.part_id, p.id)
 
     def test_inventory_query_3(self):
+        TestDatabaseInventory.inventory_creation("BARCODE", "QWERTY1234")
+        self.assertEqual(Inventory.query.count(), 1)
+        TestDatabaseInventory.inventory_creation("BARCODE 2", "HELLO1234")
+        self.assertEqual(len(Part.query.filter(Part.barcode == "HELLO1234").all()), 1)
+        self.assertEqual(Inventory.query.count(), 2)
+
         p = Part.query.filter(Part.barcode == "HELLO1234").first()
-
         i = Inventory.retrieve_inventory_from_barcode("HELLO1234")
-
         self.assertEqual(i.part_id, p.id)
 
     def test_inventory_query_4(self):
+        TestDatabaseInventory.inventory_creation("BARCODE", "QWERTY1234")
+        self.assertEqual(Inventory.query.count(), 1)
+        TestDatabaseInventory.inventory_creation("BARCODE 2", "HELLO1234")
+        self.assertEqual(len(Part.query.filter(Part.barcode == "HELLO1234").all()), 1)
+        self.assertEqual(Inventory.query.count(), 2)
+
         Inventory.query.delete()
         self.assertEqual(Inventory.query.count(), 0)
 
