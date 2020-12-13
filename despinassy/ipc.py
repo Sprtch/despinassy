15d13ead6bdfa54b2753a391010ad9dcf4a37cce
Thomas Perale <perale.thomas@gmail.com>
2020-12-13 11:52:45 +0100

    Upgrade the IPC message format

--- despinassy/ipc.py
+++ despinassy/ipc.py
@@ -1,17 +1,50 @@
-from collections import namedtuple
+import dataclasses
+from enum import Enum
+from typing import Optional
 
-IpcPrintMessage = namedtuple('PrintMessage', 'barcode, origin, name, number', defaults=(None, None, '', 1))
+class IpcOrigin(Enum):
+    UNDEFINED = 0
+    TEST = 1
+    HURON = 2
+    ERIE = 3
+
+@dataclasses.dataclass
+class IpcPrintMessage:
+    barcode: str = ""
+    origin: IpcOrigin = IpcOrigin.UNDEFINED
+    device: Optional[str] = None
+    name: str = ""
+    number: int = 1
+
+    def __post_init__(self):
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if hasattr(field.type, "__args__") and len(field.type.__args__) == 2 and field.type.__args__[-1] is type(None):
+                if value is not None and not isinstance(value, field.type.__args__[0]):
+                    raise ValueError(f'Expected {field.name} to be either {field.type.__args__[0]} or None')
+            elif not isinstance(value, field.type):
+                raise ValueError(f'Expected {field.name} to be {field.type}, '
+                                f'got {repr(value)}')
+
+    def _asdict(self):
+        return dataclasses.asdict(self)
 
 def create_nametuple(target, instance, **kwargs):
     msg_dict = {}
+    fields = []
+    if hasattr(target, '_fields'):
+        fields = target._fields
+    else:
+        fields = list(map(lambda x: x.name, dataclasses.fields(target)))
+
     if isinstance(instance, dict):
-        for f in target._fields:
+        for f in fields:
             if f in instance:
                 msg_dict[f] = kwargs.get(f) or instance[f]
             elif f in kwargs:
                 msg_dict[f] = kwargs.get(f)
     else:
-        for f in target._fields:
+        for f in fields:
             if hasattr(instance, f):
                 msg_dict[f] = kwargs.get(f) or getattr(instance, f)
             elif f in kwargs:
--- test/test_ipc.py
+++ test/test_ipc.py
@@ -1,39 +1,39 @@
 import unittest
-from despinassy.ipc import IpcPrintMessage, ipc_create_print_message, redis_subscribers_num
+from despinassy.ipc import IpcOrigin, IpcPrintMessage, ipc_create_print_message, redis_subscribers_num
 from collections import namedtuple
 import dataclasses
 
 class TestRedis(unittest.TestCase):
     def test_msg_cast1(self):
         TestMessage = namedtuple('PrintMessage', 'barcode, origin, redis, name')
-        msg = TestMessage(barcode="1234", origin="here", redis="redis", name="1234")
+        msg = TestMessage(barcode="1234", origin=IpcOrigin.TEST, redis="redis", name="1234")
         a = ipc_create_print_message(msg)
 
         self.assertTrue(isinstance(a, IpcPrintMessage))
         self.assertEqual(a.barcode, "1234")
-        self.assertEqual(a.origin, "here")
+        self.assertEqual(a.origin, IpcOrigin.TEST)
         self.assertEqual(a.name, "1234")
         self.assertEqual(a.number, 1)
 
     def test_msg_cast2(self):
         TestMessage = namedtuple('PrintMessage', 'barcode, origin, redis, name')
-        msg = TestMessage(barcode="1234", origin="here", redis="redis", name="1234")
+        msg = TestMessage(barcode="1234", origin=IpcOrigin.TEST, redis="redis", name="1234")
         a = ipc_create_print_message(msg, name="New Name")
 
         self.assertTrue(isinstance(a, IpcPrintMessage))
         self.assertEqual(a.barcode, "1234")
-        self.assertEqual(a.origin, "here")
+        self.assertEqual(a.origin, IpcOrigin.TEST)
         self.assertEqual(a.name, "New Name")
         self.assertEqual(a.number, 1)
 
     def test_msg_cast3(self):
         TestMessage = namedtuple('PrintMessage', 'barcode, origin, redis, name')
-        msg = dict(barcode="1234", origin="here", redis="redis", name="1234")
+        msg = dict(barcode="1234", origin=IpcOrigin.TEST, redis="redis", name="1234")
         a = ipc_create_print_message(msg)
 
         self.assertTrue(isinstance(a, IpcPrintMessage))
         self.assertEqual(a.barcode, "1234")
-        self.assertEqual(a.origin, "here")
+        self.assertEqual(a.origin, IpcOrigin.TEST)
         self.assertEqual(a.name, "1234")
         self.assertEqual(a.number, 1)
 
@@ -42,12 +42,12 @@ class TestRedis(unittest.TestCase):
 
         self.assertTrue(isinstance(msg, IpcPrintMessage))
         self.assertEqual(msg.barcode, "hello")
-        self.assertEqual(msg.origin, '')
+        self.assertEqual(msg.origin, IpcOrigin.UNDEFINED)
         self.assertEqual(msg.name, '')
         self.assertEqual(msg.number, 1)
 
     def test_msg_dict1(self):
-        msgdict = dict(barcode="barcode", name="name", origin="test", number=1)
+        msgdict = dict(barcode="barcode", name="name", origin=IpcOrigin.TEST, number=1, device=None)
         msg = ipc_create_print_message({}, **msgdict)
         self.assertEqual(msgdict, msg._asdict())
 
