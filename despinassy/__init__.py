9fc6628411966e5d60e739b57b640842cbd2b70a
Thomas Perale <perale.thomas@gmail.com>
2020-12-30 15:32:02 +0100

    add scanner to the module init file

--- despinassy/__init__.py
+++ despinassy/__init__.py
@@ -2,3 +2,4 @@ from despinassy.db import db
 from despinassy.Part import Part
 from despinassy.Inventory import Inventory
 from despinassy.Printer import Printer
+from despinassy.Scanner import Scanner
