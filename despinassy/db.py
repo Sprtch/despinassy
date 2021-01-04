dc873a3ea979c1e065a40418179d3f5acdf3357a
Thomas Perale <perale.thomas@gmail.com>
2021-01-04 23:14:23 +0100

    linting and database address default change

--- despinassy/db.py
+++ despinassy/db.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy.model import Model
 from functools import update_wrapper
 
+
 def setupmethod(f):
     def wrapper_func(self, *args, **kwargs):
         return f(self, *args, **kwargs)
+
     return update_wrapper(wrapper_func, f)
+
 
 class NO_APP:
     extensions = {
@@ -16,17 +19,19 @@ class NO_APP:
         'SQLALCHEMY_DATABASE_URI': None,
         'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     }
-    root_path=''
+    root_path = ''
 
     def __init__(self, config={}):
         self.teardown_appcontext_funcs = []
         self.debug = config.setdefault('debug', False)
-        NO_APP.config['SQLALCHEMY_DATABASE_URI'] = config.setdefault('uri', 'sqlite:///../huron/database.db')
+        NO_APP.config['SQLALCHEMY_DATABASE_URI'] = config.setdefault(
+            'uri', 'postgresql://postgres@localhost/sprtch')
 
     @setupmethod
     def teardown_appcontext(self, f):
         self.teardown_appcontext_funcs.append(f)
         return f
+
 
 class Despinassy(SQLAlchemy):
     def __init__(self):
@@ -46,5 +51,6 @@ class Despinassy(SQLAlchemy):
             super().init_app(app)
         else:
             super().init_app(app)
+
 
 db = Despinassy()
