1d4f1bcbe7703c38f90af1598dc7f18aad4e3a5f
Thomas Perale <perale.thomas@gmail.com>
2021-03-01 17:33:43 +0100

    linting inventory

--- despinassy/Inventory.py
+++ despinassy/Inventory.py
@@ -10,57 +10,64 @@ class InventorySession(db.Model):
     __tablename__ = "inventory_session"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    entries = relationship('Inventory', back_populates='session')
+    entries = relationship("Inventory", back_populates="session")
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 
 class Inventory(db.Model):
-    __tablename__ = 'inventory'
+    __tablename__ = "inventory"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     quantity = db.Column(db.Integer, default=0)
-    part_id = db.Column(db.Integer, db.ForeignKey('part.id'))
-    part = relationship('Part')
-    session_id = db.Column(db.Integer, db.ForeignKey('inventory_session.id'))
-    session = relationship('InventorySession')
+    part_id = db.Column(db.Integer, db.ForeignKey("part.id"))
+    part = relationship("Part")
+    session_id = db.Column(db.Integer, db.ForeignKey("inventory_session.id"))
+    session = relationship("InventorySession")
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
 
-    @validates('session')
+    @validates("session")
     def validate_redis(self, key, value):
         return InventorySession.query.order_by(
-            InventorySession.created_at.desc()).first()
+            InventorySession.created_at.desc()
+        ).first()
 
     def __repr__(self):
         return "<Inventory id=%i count=%i barcode='%s'>" % (
-            self.id, self.counter, self.part.barcode)
+            self.id,
+            self.counter,
+            self.part.barcode,
+        )
 
     def add(self, number=1):
         self.quantity += int(number)
 
     def to_dict(self):
-        return {
-            'id': self.id,
-            'part': self.part.to_dict(),
-            'quantity': self.quantity
-        }
+        return {"id": self.id, "part": self.part.to_dict(), "quantity": self.quantity}
 
     @staticmethod
     def retrieve_inventory_from_barcode(barcode):
-        return db.session.query(Inventory).join(Part).filter(
-            Part.barcode == barcode).first()
+        return (
+            db.session.query(Inventory)
+            .join(Part)
+            .filter(Part.barcode == barcode)
+            .first()
+        )
 
     @staticmethod
     def _export_csv(delimiter=","):
         strio = io.StringIO(newline=None)
         columns = [
-            "id", "part_name", "part_barcode", "quantity", "created_at",
-            "updated_at"
+            "id",
+            "part_name",
+            "part_barcode",
+            "quantity",
+            "created_at",
+            "updated_at",
         ]
-        writer = csv.DictWriter(strio,
-                                fieldnames=columns,
-                                delimiter=delimiter,
-                                lineterminator='\n')
+        writer = csv.DictWriter(
+            strio, fieldnames=columns, delimiter=delimiter, lineterminator="\n"
+        )
         writer.writeheader()
         for i in Inventory.query.all():
             row = {
@@ -77,5 +84,5 @@ class Inventory(db.Model):
 
     @staticmethod
     def export_csv(path):
-        with open(path, 'w') as csvfile:
+        with open(path, "w") as csvfile:
             csvfile.write(Inventory._export_csv().getvalue())
