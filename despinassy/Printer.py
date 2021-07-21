76a21d0446bc6e6cc55efd10f4d87cfd02273f3b
Thomas Perale <perale.thomas@gmail.com>
2021-07-21 12:01:29 +0200

    support the new ipc message structure

--- despinassy/Printer.py
+++ despinassy/Printer.py
@@ -1,5 +1,5 @@
 from despinassy.db import db
-from despinassy.ipc import IpcOrigin
+from despinassy.ipc import IpcOrigin, IpcMessageType
 from despinassy.Channel import Channel
 from sqlalchemy.orm import relationship, validates
 from sqlalchemy.exc import IntegrityError
@@ -31,7 +31,7 @@ class PrinterTypeEnum(IntEnum):
 
 
 class Printer(db.Model):
-    __tablename__ = 'printer'
+    __tablename__ = "printer"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     type = db.Column(db.Enum(PrinterTypeEnum), nullable=False)
@@ -40,18 +40,20 @@ class Printer(db.Model):
     height = db.Column(db.Integer)
     dialect = db.Column(db.Enum(PrinterDialectEnum), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    redis_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
-    redis = relationship('Channel')
+    redis_id = db.Column(db.Integer, db.ForeignKey("channel.id"))
+    redis = relationship("Channel")
     settings = db.Column(db.JSON)
-    transactions = relationship('PrinterTransaction', 
-            order_by="desc(PrinterTransaction.created_at)",
-            back_populates='printer')
+    transactions = relationship(
+        "PrinterTransaction",
+        order_by="desc(PrinterTransaction.created_at)",
+        back_populates="printer",
+    )
 
     hidden = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
 
-    @validates('redis')
+    @validates("redis")
     def validate_redis(self, key, value):
         c = Channel.query.filter(Channel.name == value)
         if c.count():
@@ -69,34 +71,34 @@ class Printer(db.Model):
     def to_dict(self, full=False):
         if full:
             return {
-                'id': self.id,
-                'type': self.type,
-                'available': self.available,
-                'width': self.width,
-                'height': self.height,
-                'dialect': self.dialect,
-                'name': self.name,
-                'redis': str(self.redis),
-                'settings': json.loads(self.settings),
-                'transactions': [t.to_dict() for t in self.transactions],
-                'created_at': self.created_at,
-                'updated_at': self.updated_at,
-                'hidden': self.hidden,
+                "id": self.id,
+                "type": self.type,
+                "available": self.available,
+                "width": self.width,
+                "height": self.height,
+                "dialect": self.dialect,
+                "name": self.name,
+                "redis": str(self.redis),
+                "settings": json.loads(self.settings),
+                "transactions": [t.to_dict() for t in self.transactions],
+                "created_at": self.created_at,
+                "updated_at": self.updated_at,
+                "hidden": self.hidden,
             }
         else:
             return {
-                'id': self.id,
-                'type': self.type,
-                'available': self.available,
-                'width': self.width,
-                'height': self.height,
-                'dialect': self.dialect,
-                'name': self.name,
-                'redis': str(self.redis),
-                'settings': json.loads(self.settings),
-                'created_at': self.created_at,
-                'updated_at': self.updated_at,
-                'hidden': self.hidden,
+                "id": self.id,
+                "type": self.type,
+                "available": self.available,
+                "width": self.width,
+                "height": self.height,
+                "dialect": self.dialect,
+                "name": self.name,
+                "redis": str(self.redis),
+                "settings": json.loads(self.settings),
+                "created_at": self.created_at,
+                "updated_at": self.updated_at,
+                "hidden": self.hidden,
             }
 
     def add_transaction(self, **kwargs):
@@ -106,31 +108,38 @@ class Printer(db.Model):
 
     def __repr__(self):
         return "<Printer id=%i type=%i name='%s' redis='%s' settings='%s'>" % (
-            self.id, self.type, self.name, str(self.redis), self.settings)
+            self.id,
+            self.type,
+            self.name,
+            str(self.redis),
+            self.settings,
+        )
 
 
 class PrinterTransaction(db.Model):
-    __tablename__ = 'printer_transaction'
+    __tablename__ = "printer_transaction"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    printer_id = db.Column(db.Integer, db.ForeignKey('printer.id'))
-    printer = relationship('Printer')
+    printer_id = db.Column(db.Integer, db.ForeignKey("printer.id"))
+    printer = relationship("Printer")
     # part_id = db.Column(db.Integer, db.ForeignKey('part.id'), unique=True)
     # part = relationship('Part')
+    destination = db.Column(db.String(50))
+    origin = db.Column(db.Enum(IpcOrigin), nullable=False)
+    msg_type = db.Column(db.Integer, default=IpcMessageType.PRINT)
+    device = db.Column(db.String(50))
     barcode = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(120), nullable=False)
     number = db.Column(db.Integer, default=1)
-    origin = db.Column(db.Enum(IpcOrigin), nullable=False)
-    device = db.Column(db.String(50))
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'barcode': self.barcode,
-            'name': self.name,
-            'number': self.number,
-            'origin': self.origin,
-            'device': self.device,
-            'created_at': self.created_at,
+            "id": self.id,
+            "barcode": self.barcode,
+            "name": self.name,
+            "number": self.number,
+            "origin": self.origin,
+            "device": self.device,
+            "created_at": self.created_at,
         }
