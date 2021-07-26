1d9737b7c5468ce8660c6b173190592279b76024
Thomas Perale <perale.thomas@gmail.com>
2021-07-26 21:27:16 +0200

    add documentation to the printer table

--- despinassy/Printer.py
+++ despinassy/Printer.py
@@ -9,12 +9,23 @@ import json
 
 
 class PrinterDialectEnum(IntEnum):
+    """
+    List the currently supported printer dialect for printer device to output.
+    """
+
     UNDEFINED = 0
+    """Not defined dialect"""
     ZEBRA_ZPL = 1
+    """The Zebra ZPL printing language"""
     TEST_JSON = 2
+    """Output as JSON object"""
 
     @staticmethod
     def from_extension(extension: str):
+        """Return dialect from file extension.
+
+        :param extension: String representing the extension of the dialect.
+        """
         if extension == "zpl":
             return PrinterDialectEnum.ZEBRA_ZPL
         elif extension == "json":
@@ -24,32 +35,77 @@ class PrinterDialectEnum(IntEnum):
 
 
 class PrinterTypeEnum(IntEnum):
+    """
+    List the currently supported type of printer device.
+    """
+
     UNDEFINED = 0
+    """Not defined printer"""
     STDOUT = 1
+    """Print to the terminal"""
     TEST = 2
+    """Printer type used only on test case"""
     STATIC = 3
+    """Network printer with a static IP address"""
 
 
 class Printer(db.Model):
+    """
+    The `Printer` model code.
+
+    Printers entry are devices that can output parts in a defined dialect.
+    This model holds the information about this output device.
+    A `Printer` can either be something virtual that will just output the
+    result to a console or a physical device like a Zebra sticker printer.
+    """
+
     __tablename__ = "printer"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     type = db.Column(db.Enum(PrinterTypeEnum), nullable=False)
+    """
+    Type of printer device. See :class:`despinassy.Printer.PrinterTypeEnum` for
+    more information.
+    """
+
     available = db.Column(db.Boolean)
+    """
+    Whether or not the `Printer` is currently available to print something.
+    For instance if a printer of type `PrinterTypeEnum.STATIC` is not connected
+    this boolean will be listed as false.
+    """
+
     width = db.Column(db.Integer)
+    """Width of the output"""
     height = db.Column(db.Integer)
+    """Height of the output"""
+
     dialect = db.Column(db.Enum(PrinterDialectEnum), nullable=False)
+    """
+    Print form of the output of the printer.
+    See :class:`despinassy.Printer.PrinterDialectEnum` for more information.
+    """
+
     name = db.Column(db.String(50), nullable=False)
+    """User defined common name for this printer"""
+
     redis_id = db.Column(db.Integer, db.ForeignKey("channel.id"))
     redis = relationship("Channel")
+
     settings = db.Column(db.JSON)
+    """Settings dependant on printer type"""
+
     transactions = relationship(
         "PrinterTransaction",
         order_by="desc(PrinterTransaction.created_at)",
         back_populates="printer",
     )
+    """List of transaction sent to this printer"""
 
     hidden = db.Column(db.Boolean, default=False)
+    """Is the printer hidden to the user."""
+
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
 
@@ -102,6 +158,12 @@ class Printer(db.Model):
             }
 
     def add_transaction(self, **kwargs):
+        """Helper to create a new :class:`despinassy.Printer.PrinterTransaction`
+
+        Someone should always use this helper function to create a new
+        :class:`despinassy.Printer.PrinterTransaction` instead of creating
+        one by hand.
+        """
         self.updated_at = datetime.datetime.utcnow()
         pt = PrinterTransaction(printer=self, **kwargs)
         return pt
@@ -117,20 +179,53 @@ class Printer(db.Model):
 
 
 class PrinterTransaction(db.Model):
+    """
+    The `PrinterTransaction` model code representing the messages sent
+    to a :class:`despinassy.Printer.Printer`.
+
+    The transaction of a printer can either be control messages or print query
+    to output content like parts from the printer.
+    """
+
     __tablename__ = "printer_transaction"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
     printer_id = db.Column(db.Integer, db.ForeignKey("printer.id"))
     printer = relationship("Printer")
+    """:class:`despinassy.Printer.Printer` where the transaction happened"""
+
     # part_id = db.Column(db.Integer, db.ForeignKey('part.id'), unique=True)
     # part = relationship('Part')
+
     destination = db.Column(db.String(50))
+
     origin = db.Column(db.Enum(IpcOrigin), nullable=False)
-    msg_type = db.Column(db.Integer, default=IpcMessageType.PRINT)
+    """
+    Device that created this transaction.
+    See :class:`despinassy.ipc.IpcOrigin` for more information.
+    """
+
     device = db.Column(db.String(50))
+    """
+    String precising the origin of the originator of the transaction.
+    """
+
+    msg_type = db.Column(db.Integer, default=IpcMessageType.PRINT)
+    """
+    Type of the message received by the printer.
+    See :class:`despinassy.ipc.IpcOrigin` for more information.
+    """
+
     barcode = db.Column(db.String(50), nullable=False)
+    """Barcode of the part the message refer to"""
+
     name = db.Column(db.String(120), nullable=False)
+    """Name of the part the message refer to"""
+
     number = db.Column(db.Integer, default=1)
+    """Number of output required by the printer"""
+
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def to_dict(self):
