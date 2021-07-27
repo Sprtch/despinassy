b63746ba1e14ebfe4de3a3f10ca3bd788105fec7
Thomas Perale <perale.thomas@gmail.com>
2021-07-27 21:05:19 +0200

    linting

--- despinassy/Channel.py
+++ despinassy/Channel.py
@@ -4,17 +4,17 @@ import datetime
 
 
 class Channel(db.Model):
-    __tablename__ = 'channel'
+    __tablename__ = "channel"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), unique=True, index=True)
-    printers = relationship('Printer', back_populates='redis')
-    scanners = relationship('Scanner', back_populates='redis')
+    printers = relationship("Printer", back_populates="redis")
+    scanners = relationship("Scanner", back_populates="redis")
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
 
     def __repr__(self):
-        return '<Channel %r>' % (self.name)
+        return "<Channel %r>" % (self.name)
 
     def __print__(self):
         return self.name
@@ -24,6 +24,6 @@ class Channel(db.Model):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
+            "id": self.id,
+            "name": self.name,
         }
