108051c64d793666a1defa6dc4cdab7004943cb4
Thomas Perale <perale.thomas@gmail.com>
2021-07-28 20:15:28 +0200

    linting

--- test/test_printer.py
+++ test/test_printer.py
@@ -2,16 +2,23 @@ import unittest
 import json
 from despinassy import db
 from despinassy.ipc import IpcPrintMessage, IpcOrigin
-from despinassy.Printer import Printer, PrinterTransaction, PrinterDialectEnum, PrinterTypeEnum
+from despinassy.Printer import (
+    Printer,
+    PrinterTransaction,
+    PrinterDialectEnum,
+    PrinterTypeEnum,
+)
 from despinassy.Channel import Channel
 
 
 class TestDatabasePrinter(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        db.init_app(config={
-            'uri': 'sqlite://',
-        })
+        db.init_app(
+            config={
+                "uri": "sqlite://",
+            }
+        )
         db.drop_all()
 
     def setUp(self):
@@ -21,30 +28,33 @@ class TestDatabasePrinter(unittest.TestCase):
         db.drop_all()
 
     def test_printer_creation(self):
-        p = Printer(name="main",
-                    type=1,
-                    dialect=1,
-                    redis="victoria",
-                    settings='{"address": "192.168.0.1"}')
+        p = Printer(
+            name="main",
+            type=1,
+            dialect=1,
+            redis="victoria",
+            settings='{"address": "192.168.0.1"}',
+        )
         db.session.add(p)
         db.session.commit()
         self.assertEqual(Printer.query.count(), 1)
         self.assertEqual(p.type, PrinterTypeEnum.STDOUT)
         self.assertEqual(p.dialect, PrinterDialectEnum.ZEBRA_ZPL)
         self.assertEqual(Printer.query.get(p.id), p)
-        self.assertEqual(json.loads(p.settings)['address'], "192.168.0.1")
+        self.assertEqual(json.loads(p.settings)["address"], "192.168.0.1")
 
     def test_printer_transaction(self):
-        p = Printer(name="main",
-                    type=1,
-                    dialect=1,
-                    redis="victoria",
-                    settings='{"address": "192.168.0.1"}')
+        p = Printer(
+            name="main",
+            type=1,
+            dialect=1,
+            redis="victoria",
+            settings='{"address": "192.168.0.1"}',
+        )
         db.session.add(p)
-        pt = PrinterTransaction(printer=p,
-                                name="bar",
-                                barcode="FOO",
-                                origin=IpcOrigin.TEST)
+        pt = PrinterTransaction(
+            printer=p, name="bar", barcode="FOO", origin=IpcOrigin.TEST
+        )
         db.session.add(pt)
         db.session.commit()
         self.assertEqual(Printer.query.count(), 1)
@@ -53,15 +63,20 @@ class TestDatabasePrinter(unittest.TestCase):
         self.assertEqual(p.transactions[0], pt)
 
     def test_printer_add_transaction(self):
-        msg = IpcPrintMessage(barcode="hello",
-                              name="world",
-                              origin=IpcOrigin.TEST,
-                              number=1)
-        p = Printer(name="main",
-                    type=1,
-                    dialect=1,
-                    redis="victoria",
-                    settings='{"address": "192.168.0.1"}')
+        msg = IpcPrintMessage(
+            barcode="hello",
+            name="world",
+            origin=IpcOrigin.TEST,
+            destination="victoria",
+            number=1,
+        )
+        p = Printer(
+            name="main",
+            type=1,
+            dialect=1,
+            redis="victoria",
+            settings='{"address": "192.168.0.1"}',
+        )
         db.session.add(p)
         pt = p.add_transaction(**msg._asdict())
         db.session.add(pt)
@@ -74,22 +89,20 @@ class TestDatabasePrinter(unittest.TestCase):
     def test_printer_transaction_order(self):
         """
         Verify the order of the 'Printer' table 'transactions' field.
-        The last added transactions should be the first one in the 
+        The last added transactions should be the first one in the
         transaction list.
         """
-        p = Printer(name="main",
-                    type=1,
-                    dialect=1,
-                    redis="victoria",
-                    settings='{"address": "192.168.0.1"}')
+        p = Printer(
+            name="main",
+            type=1,
+            dialect=1,
+            redis="victoria",
+            settings='{"address": "192.168.0.1"}',
+        )
         db.session.add(p)
-        pt1 = p.add_transaction(origin=IpcOrigin.TEST,
-                                barcode="foo",
-                                name="bar")
+        pt1 = p.add_transaction(origin=IpcOrigin.TEST, barcode="foo", name="bar")
         db.session.add(pt1)
-        pt2 = p.add_transaction(origin=IpcOrigin.TEST,
-                                barcode="hello",
-                                name="world")
+        pt2 = p.add_transaction(origin=IpcOrigin.TEST, barcode="hello", name="world")
         db.session.add(pt2)
         db.session.commit()
         self.assertEqual(len(p.transactions), 2)
@@ -97,5 +110,5 @@ class TestDatabasePrinter(unittest.TestCase):
         self.assertEqual(p.transactions[1].name, "bar")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
